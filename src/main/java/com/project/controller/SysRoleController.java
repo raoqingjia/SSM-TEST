0759dddd499b93124dac0a5ed9465f0f33f61693
raoqingjia <1007804453@qq.com>
2019-08-14 18:51:55 +0800

    提交代码

--- src/main/java/com/project/controller/SysRoleController.java
+++ src/main/java/com/project/controller/SysRoleController.java
@@ -1,9 +1,6 @@
 package com.project.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +12,31 @@ import com.project.entity.SysRole;
 import com.project.service.SysRoleService;
 
 @Controller
-@RequestMapping("/req")
+@RequestMapping("/pages")
 public class SysRoleController  {
 	@Autowired
 	private SysRoleService sysRoleService;
 	
-   @RequestMapping("/hello")
-   public ModelAndView Hello(ModelAndView mv){
-	   mv.setViewName("helloPage");
-	   mv.addObject("msg", "hello world!!!");
+	  @RequestMapping("/hello")
+	   public ModelAndView Hello(ModelAndView mv){
+		   System.out.println("222");
+		   mv.setViewName("helloPage");
+		return mv;
+	   }
+	  
+   @RequestMapping("/index")
+   public ModelAndView Index(ModelAndView mv){
+	   System.out.println("555");
+	   mv.setViewName("index");
 	return mv;
    }
-
-   @RequestMapping("/findObject")
+   @RequestMapping("/getLoad")
+   public ModelAndView Load(ModelAndView mv){
+	   System.out.println("444");
+	   mv.setViewName("load");
+	return mv;
+   }
+   @RequestMapping("/findSysRoleObject")
    @ResponseBody
    @JsonIgnoreProperties(value = { "hibernateLazyInitializer", "handler" })
    public List<SysRole> testJson(ModelAndView mv){
